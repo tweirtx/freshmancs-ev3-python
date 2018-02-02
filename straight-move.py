bb7db0855e4d36399e11d8cf74e836e1b0f5cfcb
Travis <tweirtx@gmail.com>
2018-02-02 09:43:21 -0600

    Straight move and curved move

---
+++ straight-move.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import time
+
+from ev3dev import ev3
+leftmotor = ev3.LargeMotor('outA')
+rightmotor = ev3.LargeMotor('outB')
+
+leftmotor.run_forever(speed_sp=500)
+rightmotor.run_forever(speed_sp=500)
+time.sleep(5)
+leftmotor.stop()
+rightmotor.stop()
