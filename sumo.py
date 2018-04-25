a897f2b247f59b880e1aeb0c40e9340842c711b7
Travis Weir <tweirtx@gmail.com>
2018-04-25 10:09:35 -0500

    Made it a little fancier

--- sumo.py
+++ sumo.py
@@ -6,6 +6,7 @@ stopbutton = ev3.TouchSensor()
 left = ev3.LargeMotor('outA')
 right = ev3.LargeMotor('outB')
 sensor.mode = sensor.MODE_US_DIST_IN
+lights = ev3.Leds()
 
 print("Initialized! Press enter to start fighting")
 input("Press enter")
@@ -14,12 +15,18 @@ while True:
     distance = sensor.distance_inches
     if distance < 6:
         print("Robot detected, ATTACKING!")
+        lights.red_left
+        lights.red_right
         right.run_forever(speed_sp=1000)
         left.run_forever(speed_sp=1000)
     else:
         print("No robot detected, spinning")
+        lights.set_color(lights.LEFT, lights.ORANGE)
+        lights.set_color(lights.RIGHT, lights.ORANGE)
         right.run_forever(speed_sp=1000)
         left.run_forever(speed_sp=-1000)
     if stopbutton.is_pressed:
         print("Stop button pressed")
+        lights.green_left
+        lights.green_right
         break
