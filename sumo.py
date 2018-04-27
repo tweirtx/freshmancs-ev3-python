99a61919fcbaf840667e051c15342b38bbfeb3d2
Travis Weir <tweirtx@gmail.com>
2018-04-27 08:51:45 -0500

    Added code to run the flippyboi

--- sumo.py
+++ sumo.py
@@ -5,6 +5,7 @@ sensor = ev3.UltrasonicSensor()
 stopbutton = ev3.TouchSensor()
 left = ev3.LargeMotor('outA')
 right = ev3.LargeMotor('outB')
+flippyboi = ev3.MediumMotor('outC')
 sensor.mode = sensor.MODE_US_DIST_IN
 lights = ev3.Leds()
 
@@ -19,6 +20,8 @@ while True:
         lights.red_right
         right.run_forever(speed_sp=1000)
         left.run_forever(speed_sp=1000)
+        flippyboi.run_to_rel_pos(speed_sp=1000, position_sp=300)
+        flippyboi.run_to_rel_pos(speed_sp=1000, position_sp=-300)
     else:
         print("No robot detected, spinning")
         lights.set_color(lights.LEFT, lights.ORANGE)
